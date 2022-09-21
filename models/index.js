8a5f0c88c286a9bf57f5f74a27bf201e463cfde6
sdott3 <sdotthomas347@gmail.com>
2022-09-21 16:48:13 -0400

    product, stock and category column added to product.js

--- models/index.js
+++ models/index.js
@@ -5,11 +5,20 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL'
+}
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  // as: 'product_tag',
+  foreignKey: 'product_id'
+})
 // Tags belongToMany Products (through ProductTag)
 
 module.exports = {
